fe94fe3f30b541c05c920e295ed672da2c2136f4
Atif-Sheikh <Atifsiddiquissg@gmail.com>
2021-05-06 17:41:44 +0500

    Implement login and signup loading

--- src/components/inputForm/inputForm.js
+++ src/components/inputForm/inputForm.js
@@ -7,7 +7,7 @@ import { Button } from "@chakra-ui/react";
 import InputField from '../inputField/inputField';
 import './inputForm.css';
 
-const InputForm = ({ type, onClickBtn }) => {
+const InputForm = ({ type, onClickBtn, isLoading = false }) => {
     const [name, setName] = useState('');
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
@@ -22,7 +22,7 @@ const InputForm = ({ type, onClickBtn }) => {
                 {type === 'signup' && <InputField value={name} onChangeHandler={({ target: { value } }) => setName(value)} label="First name" />}
                 <InputField value={email} onChangeHandler={({ target: { value } }) => setEmail(value)} label="Email" />
                 <InputField value={password} onChangeHandler={({ target: { value } }) => setPassword(value)} label="Password" />
-                <Button onClick={handleClick} className="createAccount" colorScheme="blue">{type === 'signup' ? "Create my account" : "Login"}</Button>
+                <Button isLoading={isLoading} onClick={handleClick} className="createAccount" colorScheme="blue">{type === 'signup' ? "Create my account" : "Login"}</Button>
             </Stack>
         </div>
     )
