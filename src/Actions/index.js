f325ef2a54a2780f7eb7e77ce763f4548f4e6487
Atif-Sheikh <atifsiddiquissg@gmail.com>
2021-06-02 16:47:38 +0500

    Update changes

---
+++ src/Actions/index.js
@@ -0,0 +1,53 @@
+import firebase from 'firebase/app';
+import FirebaseService from 'Utils/firebaseService';
+import {store} from '../index';
+
+export const getMessages = (path) => {
+        firebase.database().ref(path).orderByChild('time').on("value", (snapshot) => {
+            store.dispatch({type: "CHAT_MESSAGES", payload: snapshot.val() })
+        });
+}
+
+export const sendTyping = (path, data) => {
+    return firebase.database().ref(path).set(data);
+} 
+
+export const removeTyping = path => {
+    return firebase.database().ref(path).remove();
+};
+
+
+export const sendMessage = (path, data) => {
+    return firebase.database().ref(path).push(data);
+}
+
+export const getUser = async (path) => {
+    let user = await FirebaseService.getOnceFromDatabase(path)
+    store.dispatch({type: "GET_USER", payload: user.val() })
+}
+
+export const getGroupMessages = (path) => {
+    firebase.database().ref(path).orderByChild('time').on("value", (snapshot) => {
+        store.dispatch({type: "GROUP_MESSAGES", payload: snapshot.val() })
+    });
+}
+
+export const changeMessage = (path, data) => {
+    return firebase.database().ref(path).set(data);
+}
+
+export const typingStop = (path) => {
+    return firebase.database().ref(path).remove();
+};
+
+export const tokenToGroup = (path, data) => {
+    return firebase.database().ref(path).update(data);
+};
+
+export const joinGroupAction = (path, data) => {
+    return firebase.database().ref(path).set(data)
+};
+
+export const groupEntry = (path) => {
+    return FirebaseService.getOnceFromDatabase(path);
+};
